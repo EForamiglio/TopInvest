d19ce57e266e25139b2589ec094df4a782067fb7
eloatorres <eloa.torres.cunha@hotmail.com>
2024-05-21 23:56:39 -0300

    inclusão do sobre

---
+++ Controllers/SobreController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TopInvest.Controllers
+{
+    public class SobreController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (!HelperController.VerificaUserLogado(HttpContext.Session))
+                return RedirectToAction("Index", "Login");
+            ViewBag.Logado = HelperController.VerificaUserLogado(HttpContext.Session);
+            ViewBag.NomeUser = HelperController.PreencheNomeUser(HttpContext.Session);
+            ViewBag.Adm = HelperController.VerificaUserAdm(HttpContext.Session);
+            return View();
+        }
+    }
+}
